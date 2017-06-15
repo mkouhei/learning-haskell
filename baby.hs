b7f86aceec146871d03610ec9aee54c4392259e6
Kohei Maeda <mkouhei@palmtb.net>
2017-06-16 07:07:50 +0900

    Section 1.3 (not completed)
    Signed-off-by: Kohei Maeda <mkouhei@palmtb.net>

--- baby.hs
+++ baby.hs
@@ -11,3 +11,30 @@ doubleSmallNumber x = if x > 100
   else x * 2
 doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
 conanO'Brien = "It's a-me, Conan O'Brien!"
+
+-- Section 1.3
+--gchi> let lostNumbers = [4,8,15,16,23,42]
+lostNumbers = [4,8,15,16,23,42]
+concatNumbers = [1,2,3,4] ++ [9,10,11,12]
+concatString = "hello" ++ " " ++ "world"
+concatCharacterList = ['w', 'o'] ++ ['o', 't']
+-- "woot"
+-- ["w","o"] ++ ["o","t"]
+-- => ["w", "o", "o", "t"]
+-- ['a','b'] ++ "c"
+-- => "abc"
+
+-- cons (:)
+consOperator = 'A' : " SMALL CAT"
+-- "A ": "SMALL CAT" --> NG
+consOperatorList = 5:[1,2,3,4,5]
+syntaxSugarList = 1:2:3:[] ++ [4]
+accessElement = "Steve Buscemi" !! 6
+-- "hoge" !! -1 --> NG
+accessElementList = [9.4, 33.2, 96.2, 11.2, 23.25] !! 1
+-- [1, 2, 3] !! 3 --> NG
+
+nestedList = [[1,2,3],[4,5,6],[7,8,9,10]]
+concatNestedList = nestedList ++ [[1,2,3]]
+consNestedList = [6,6,6]: nestedList
+accessElementNestedList = nestedList !! 2
